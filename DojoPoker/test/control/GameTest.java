5a7a42a0f43a4b0aef68e7b23bfc2a95e90da236
Hanting Wang <hanting.wang@etu.unice.fr>
2018-02-26 18:42:32 +0100

    test havePair

--- DojoPoker/test/control/GameTest.java
+++ DojoPoker/test/control/GameTest.java
@@ -2,6 +2,7 @@ package control;
 
 
 import model.Card;
+import model.Player;
 import model.Rank;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -39,6 +40,7 @@ class GameTest {
     }
 
     @Test
+    // A little problem, need to be fixed, not a big deal
     void comparePair() {
         Card card1 = new Card(Rank.FOUR);
         Card card2 = new Card(Rank.TWO);
@@ -52,4 +54,23 @@ class GameTest {
         boolean expected = true;
         assertEquals(actual,expected);
     }
+
+    @Test
+    void havePair() {
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
+        Card card1 = new Card(Rank.FIVE);
+        Card card2 = new Card(Rank.TWO);
+        List<Card> handCards1 = new ArrayList<Card>();
+        List<Card> handCards2 = new ArrayList<Card>();
+        player1.addCard(card1);
+        player1.addCard(card1);
+        player2.addCard(card2);
+        player2.addCard(card2);
+        boolean actual = player1.havePair() > 1 && player1.havePair() >0;
+        boolean expected = true;
+        assertEquals(actual,expected);
+    }
+
+
 }
