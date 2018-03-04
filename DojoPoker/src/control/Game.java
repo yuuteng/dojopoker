d78095d299219a55aca7f76f4f70b03e925ae4b8
Teng Yu <teng.yu@etu.unice.fr>
2018-03-04 18:14:48 +0100

    fix bug to the final goal

--- DojoPoker/src/control/Game.java
+++ DojoPoker/src/control/Game.java
@@ -4,8 +4,12 @@ import com.sun.org.apache.regexp.internal.REUtil;
 import model.Card;
 import model.Deck;
 import model.Player;
+import model.Rank;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     private Player player1;
@@ -37,42 +41,105 @@ public class Game {
     public void compare() {
         List<Card> handCards1 = player1.getCards();
         List<Card> handCards2 = player2.getCards();
+        Map<String, List<Card>> resMap = new HashMap<>();
         int result = -1;
         String comment = "";
         if (player1.haveFlush() > 0 || player2.haveFlush() > 0) {
             result = compareFlush();
-            comment = "quinte flush";
+            comment = "quinte flush fin de ";
+            if (result == 1) {
+                comment += matchCard(player1.haveFlush());
+            } else {
+                comment += matchCard(player2.haveFlush());
+            }
         } else if (player1.haveFour() > 0 || player2.haveFour() > 0) {
             result = compareFour();
-            comment = "carré";
+            comment = "carré de ";
+            if (result == 1) {
+                comment += matchCard(player1.haveFour());
+            } else {
+                comment += matchCard(player2.haveFour());
+            }
         }else if (player1.haveFull() > 0 || player2.haveFull() > 0) {
             result = compareFull();
-            comment = "full";
+            comment = "full de trois ";
+            if (result == 1) {
+                comment += matchCard(player1.haveFull());
+            } else {
+                comment += matchCard(player2.haveFull());
+            }
         } else if (player1.haveSameColor() > 0 || player2.haveSameColor() > 0) {
             result = compareColor();
-            comment = "couleur";
+            comment = "couleur de ";
+            if (result == 1) {
+                comment += player1.getCards().get(0);
+            } else {
+                comment += player2.getCards().get(0);
+            }
         } else if (player1.haveSuit() > 0 || player2.haveSuit() > 0) {
             result = compareSuit();
-            comment = "suite";
+            comment = "suite de ";
+            if (result == 1) {
+                for (int i = 0;i<5;i++) {
+                    comment += matchCard(player1.haveSuit() - i)+" ";
+                }
+            } else {
+                for (int i = 0;i<5;i++) {
+                    comment += matchCard(player2.haveSuit() - i)+" ";
+                }
+            }
         } else if (player1.haveThree() > 0 || player2.haveThree() > 0) {
             result = compareThree();
-            comment = "brelan";
+            comment = "brelan de ";
+            if (result == 1) {
+                comment += matchCard(player1.haveThree());
+            } else {
+                comment += matchCard(player2.haveThree());
+            }
         } else if (player1.haveTwoPair() != null || player2.haveTwoPair() != null) {
             result = compareTwoPair();
-            comment = "deux paire";
+            comment = "deux paire de ";
+            List<Card> listTmp;
+            if (result == 1) {
+                listTmp = player1.haveTwoPair();
+                comment += matchCard(listTmp.get(0).getValue()) +" et " +matchCard(listTmp.get(1).getValue());
+            } else {
+                listTmp = player2.haveTwoPair();
+                comment += matchCard(listTmp.get(0).getValue()) +" et " +matchCard(listTmp.get(1).getValue());
+            }
         } else if (player1.havePair() > 0 || player2.havePair() > 0) {
             result = comparePair();
-            comment = "paire";
+            comment = "paire de ";
+            if (result == 1) {
+                comment += matchCard(player1.havePair());
+            } else {
+                comment += matchCard(player2.havePair());
+            }
         } else {
-            result = compareHighCard(handCards1, handCards2);
-            comment = "carte la plus élevée";
-        }
+            comment = "carte la plus élevée : ";
+            resMap = compareHighCard(handCards1, handCards2);
+                for (Map.Entry<String, List<Card>> entry : resMap.entrySet()) {
+                    switch (entry.getKey()) {
+                        case "1":
+                            comment += matchCard(entry.getValue().get(0).getValue());
+                            result = 1;
+                            break;
+                        case "2":
+                            comment += matchCard(entry.getValue().get(0).getValue());
+                            result = 2;
+                            break;
+                        default:
+                            result = 0;
+                            break;
+                    }
+                }
+            }
         switch (result) {
             case 1:
-                System.out.println("La main 1 gagne avec " + comment + " win");
+                System.out.println("La main 1 gagne avec " + comment);
                 break;
             case 2:
-                System.out.println("La main 1 gagne avec " + comment + " win");
+                System.out.println("La main 1 gagne avec " + comment);
                 break;
             default:
                 System.out.println("Egalite");
@@ -80,16 +147,37 @@ public class Game {
         }
     }
 
+    String matchCard(int num) {
+        switch (num) {
+            case 11:
+                return "V";
+            case 12:
+                return "D";
+            case 13:
+                return "R";
+            case 14:
+                return "As";
+            default:
+                return String.valueOf(num);
+        }
+    }
     // 0 equal 1 p1win  2 p2win
-    public int compareHighCard(List<Card> handCards1, List<Card> handCards2) {
+    public Map<String, List<Card>> compareHighCard(List<Card> handCards1, List<Card> handCards2) {
         Card card1 = handCards1.get(0);
         Card card2 = handCards2.get(0);
+        List<Card> listCard = new ArrayList<Card>();
+        Map<String, List<Card>> resMap = new HashMap<>();
         if (card1.getValue() > card2.getValue()) {
-            return 1;
+            listCard.add(card1);
+            resMap.put("1", listCard);
+            return resMap;
         } else if (card1.getValue() < card2.getValue()) {
-            return 2;
+            listCard.add(card2);
+            resMap.put("2", listCard);
+            return resMap;
         } else {
-            return 0;
+            resMap.put("0", listCard);
+            return resMap;
         }
     }
     // 0 equal 1 p1win  2 p2win
